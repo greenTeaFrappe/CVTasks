b9ef7934b4291fb267dd57a461013fddf8aff2cd
greenTeaFrappe <jk008532@gmail.com>
2024-10-13 22:29:54 +0900

    HW#2-2 : 자동차 번호판 후보 검출하기

--- Task2/Task2-1.py
+++ Task2/Task2-1.py
@@ -15,7 +15,7 @@ while True:
         print('프레임 획득에 실패하여 루프를 나갑니다.')
         break
 
-    # 기본적으로 frame1이 없다면 원본을 출력
+    # frame1이 없다면 원본을 출력
     if frame1 is None:
         cv2.imshow('Video display', frame)
     else:
@@ -25,23 +25,23 @@ while True:
 
     if key == ord('q'):
         break
-    elif key == ord('n'):
+    elif key == ord('n'):   # 오리지널
         frame1 = frame.copy()
         cv2.putText(frame1, 'Original', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 100, 100), 2)
-    elif key == ord('b'):
+    elif key == ord('b'):   # bilateral
         frame1 = cv2.bilateralFilter(frame, -1, 10, 5)
         cv2.putText(frame1, 'Bilateral', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 100, 100), 2)
-    elif key == ord('s'):
+    elif key == ord('s'):   # stylization
         frame1 = cv2.stylization(frame, sigma_s=60, sigma_r=0.45)
         cv2.putText(frame1, 'Stylization', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 100, 100), 2)
-    elif key == ord('g'):
+    elif key == ord('g'):   # Gray pencilSketch
         frame1, _ = cv2.pencilSketch(frame, sigma_s=60, sigma_r=0.7, shade_factor=0.02)
         frame1 = cv2.cvtColor(frame1, cv2.COLOR_GRAY2BGR)  # 그레이 이미지를 컬러로 변환해서 같은 창에 출력
         cv2.putText(frame1, 'Gray pencilSketch', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 100, 100), 2)
-    elif key == ord('c'):
+    elif key == ord('c'):   # Color pencilSketch
         _, frame1 = cv2.pencilSketch(frame, sigma_s=60, sigma_r=0.7, shade_factor=0.02)
         cv2.putText(frame1, 'Color pencilSketch', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 100, 100), 2)
-    elif key == ord('o'):
+    elif key == ord('o'):   # Oil Painting
         frame1 = cv2.xphoto.oilPainting(frame, 7, 1)
         cv2.putText(frame1, 'Oil Painting', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 100, 100), 2)
 
--- Task2/Task2-2.py
+++ Task2/Task2-2.py
@@ -1,4 +1,25 @@
 import cv2
 
+se2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+k = 1
 
-car=cv2.imread('00.jpg',cv2.IMREAD_GRAYSCALE)
+for i in range(0,6): # 6번 반복
+    gray=cv2.imread(f'0{i}.jpg',cv2.IMREAD_GRAYSCALE) # 이미지 가져오기
+    blur=cv2.blur(gray, (3, 3)) # 전처리
+
+    # 세로 에지 검출
+    sobel_grad=cv2.Sobel(blur,cv2.CV_32F,1,0,ksize=3)
+    sobel = cv2.convertScaleAbs(sobel_grad)
+
+    # 임계값을 이용한 검은 배경과 흰 에지 분리
+    ret, binary_img = cv2.threshold(sobel, 120, 255, cv2.THRESH_BINARY)
+
+    # 가로로 긴 구조요소를 이용한 여러번의 닫힘(close)를 통해 흰숫자 에지를 팽창한 후 원상태로 침식
+    b_closing = cv2.erode(cv2.dilate(binary_img,se2,iterations=k),se2,iterations=k)
+    b_closing = cv2.morphologyEx(b_closing, cv2.MORPH_CLOSE, se2)
+
+    # 이미지 보여줌
+    cv2.imshow(f'result{i}', b_closing)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
